7964205e5074dd0ec41802a05126d7c227a30d7e
Mazl <135579782+nhanhem2ui@users.noreply.github.com>
2026-01-20 00:23:35 +0700

    [ADD]: Login feature (#2)

---
+++ BussinessObjects/Models/GameNews.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BussinessObjects.Models
+{
+    public class GameNews
+    {
+        public Guid Id { get; set; }
+
+        [MaxLength(500)]
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string BannerPath { get; set; }
+
+        [MaxLength(500)]
+        [Required]
+        public string Description { get; set; }
+
+        [MaxLength(10000)]
+        [Required]
+        public string Content { get; set; }
+    }
+}
---
+++ BussinessObjects/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace BussinessObjects.Models
+{
+    public class User : IdentityUser<Guid>
+    {
+        [MaxLength(50)]
+        [Required]
+        public override string UserName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateOnly? UserDOB { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(11, ErrorMessage = "The phone number must be at most 11 digits.")]
+        [RegularExpression(@"^\d{1,11}$", ErrorMessage = "The phone number must only contain digits")]
+        public override string? PhoneNumber { get; set; }
+
+        [Display(Name = "Gender")]
+        public byte Gender { get; set; } // 0: male, 1: female, 3: other
+
+        [ValidateNever]
+        public string? UserAvatar { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
+
+        public bool IsBanned { get; set; }
+
+        public ICollection<UserItem> UserItems { get; set; } = new List<UserItem>();
+    }
+}
