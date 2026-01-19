a39de76310039ccc8572a778ccfab92c34a30938
Bao <nhanhem2ui@gmail.com>
2026-01-19 23:09:49 +0700

    [ADD]: Login feature

---
+++ BussinessObjects/Models/Item.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BussinessObjects.Models
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(500)]
+        [Required]
+        public string Description { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Type { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Rarity { get; set; }
+
+        [Required]
+        public string ImagePath { get; set; }
+
+        public ICollection<UserItem> UserItems { get; set; } = new List<UserItem>();
+    }
+}
---
+++ BussinessObjects/Models/UserItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BussinessObjects.Models
+{
+    public class UserItem
+    {
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public Guid ItemId { get; set; }
+        public Item Item { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
